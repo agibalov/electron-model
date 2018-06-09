020b5ac79377fc97155cfd7a6048a053f883ed50
agibalov <loki2302@loki2302.me>
2018-06-09 19:37:42 -0400

    Now using the 'classic' layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,9 @@ import {LorentzService, Sample} from './lorentz.service';
   selector: 'app-root',
   // tslint:disable:no-trailing-whitespace max-line-length
   template: `
-    <div class="columns">
-      <div class="column">
-        <canvas style="width: 100%; height: 600px;"
+    <div class="root-container">
+      <div class="top-container">
+        <canvas class="scene-view" 
                 manipulator
                 (manipulationBegin)="cameraDriver.handleManipulationBegin(three.camera)"
                 (manipulationRotationUpdate)="cameraDriver.handleRotationUpdate($event)"
@@ -25,13 +25,12 @@ import {LorentzService, Sample} from './lorentz.service';
             <axes *ngIf="showAxes"></axes>
           </scene>
         </canvas>
-      </div>
-      <div class="column is-one-third is-one-fifth-widescreen">
-        <vector-editor name="Start Velocity" [range]="1e6" [(ngModel)]="lorentzService.startVelocity"></vector-editor>
-        <vector-editor name="Electric Field" [range]="1e-5" [(ngModel)]="lorentzService.electricField"></vector-editor>
-        <vector-editor name="Magnetic Field" [range]="1e-10" [(ngModel)]="lorentzService.magneticField"></vector-editor>
+        <div class="right-panel">
+          <vector-editor name="Start Velocity" [range]="1e6" [(ngModel)]="lorentzService.startVelocity"></vector-editor>
+          <vector-editor name="Electric Field" [range]="1e-5" [(ngModel)]="lorentzService.electricField"></vector-editor>
+          <vector-editor name="Magnetic Field" [range]="1e-10" [(ngModel)]="lorentzService.magneticField"></vector-editor>
 
-        <pre class="checkboxes">
+          <pre class="checkboxes">
 
 <label class="checkbox"><input type="checkbox" [(ngModel)]="showElectron"> Show electron</label>
 <label class="checkbox"><input type="checkbox" [(ngModel)]="showTrajectory"> Show trajectory</label>
@@ -39,30 +38,61 @@ import {LorentzService, Sample} from './lorentz.service';
 <label class="checkbox"><input type="checkbox" [(ngModel)]="showAxes"> Show axes</label>
           
 <span class="has-text-weight-bold">Number of Samples</span>: <input type="range" class="slider is-small is-circle is-success"
-                                                              [min]="10" [max]="5000" [step]="1"
-                                                              [(ngModel)]="lorentzService.numberOfSamples"> ({{lorentzService.numberOfSamples}})
+                                                                    [min]="10" [max]="5000" [step]="1"
+                                                                    [(ngModel)]="lorentzService.numberOfSamples"> ({{lorentzService.numberOfSamples}})
 <span class="has-text-weight-bold">Time of Flight</span>: <input type="range" class="slider is-small is-circle is-success"
                                                                  [min]="0.1" [max]="10" [step]="1e-3"
                                                                  [(ngModel)]="lorentzService.timeOfFlight"> ({{lorentzService.timeOfFlight | exponential}})
-        </pre>
+          </pre>
+        </div>
       </div>
-    </div>
-    <div class="container is-fluid">
-      <input type="range" class="slider is-small is-fullwidth"
-             [min]="0"
-             [max]="lorentzService.trajectory.length - 1"
-             [step]="1"
-             [(ngModel)]="currentSampleIndex">
-      <p>{{currentSampleIndex + 1}} / {{lorentzService.trajectory.length}}</p>
+      <div class="bottom-container">
+        <div class="container is-fluid">
+          <input type="range" class="slider is-small is-fullwidth"
+                 [min]="0"
+                 [max]="lorentzService.trajectory.length - 1"
+                 [step]="1"
+                 [(ngModel)]="currentSampleIndex">
+          <p>{{currentSampleIndex + 1}} / {{lorentzService.trajectory.length}}</p>
 
-      <pre class="debug">
+          <pre class="debug">
 timestamp: {{currentSample.timestamp | exponential}}
 position: ({{currentSample.position.x | exponential}}, {{currentSample.position.y | exponential}}, {{currentSample.position.z | exponential}})
 velocity: ({{currentSample.velocity.x | exponential}}, {{currentSample.velocity.y | exponential}}, {{currentSample.velocity.z | exponential}})
 acceleration: ({{currentSample.acceleration.x | exponential}}, {{currentSample.acceleration.y | exponential}}, {{currentSample.acceleration.z | exponential}})</pre>
+        </div>
+      </div>
     </div>
   `,
   styles: [`
+    .root-container {
+      width: 100vw;
+      height: 100vh;
+      display: flex;
+      flex-direction: column;
+    }
+    
+    .top-container {
+      flex-grow: 1;
+      display: flex;
+      flex-direction: row;
+    }
+    
+    .scene-view {
+      flex-grow: 1;
+      width: 1%;
+    }
+    
+    .right-panel {
+      width: 400px;
+      padding-left: 5px;
+      overflow-y: auto;
+    }
+
+    .bottom-container {
+      padding-bottom: 5px;
+    }
+    
     .checkboxes {
       padding: 0;
       margin: 0;
